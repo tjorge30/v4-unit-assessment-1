91e505045a4dcb7c14b86855414cdba9aeddf9c2
tjorge30 <mrs.javalera@gmail.com>
2021-02-19 17:04:21 -0800

    completed javascript-3

--- javascript-3/practice-js-3.js
+++ javascript-3/practice-js-3.js
@@ -2,12 +2,12 @@
 /* Create an array called 'groceries' that contains three grocery items of your choice as strings. */
 
 //CODE HERE
-
+let groceries = ['milk', 'eggs', 'cheese']
 //////////////////STEP 2////////////////////
 /* Create a variable called 'yourName' that is assigned the value of your name as a string. */
 
 //CODE HERE
-
+let yourName = 'Tasha'
 //////////////////STEP 3////////////////////
 /*
     Create a function called 'setGroceryListTitle' that takes in one parameter called 'x'. 
@@ -17,7 +17,9 @@
 */
 
 //CODE HERE
-
+function setGroceryListTitle(x) {
+    return x + '\'s'+ ' Grocery List'
+}
 //////////////////STEP 4////////////////////
 /* 
     Create a function called 'addItem' that takes in one parameter called 'item'. 
@@ -27,7 +29,9 @@
 */
 
 //CODE HERE
-
+function addItem(item) {
+    groceries.push(item)
+}
 //////////////////STEP 5////////////////////
 /*
     Create a function called 'removeItem' that takes in one parameter called 'index'. 
@@ -37,7 +41,10 @@
 */
 
 //CODE HERE
-
+function removeItem(index) {
+    groceries.splice(index)
+    return displayData(removeItem)
+}
 //////////////////STEP 6////////////////////
 /*
     Create a function called 'checkGroceryCount'. Inside of checkGroceryCount, 
@@ -50,7 +57,17 @@
 */
 
 //CODE HERE
-
+function checkGroceryCount() {
+    if (groceries.length > 5) {
+        return 'That looks like a big trip';
+    } else if (groceries.length = 1) {
+        return '1 item';
+    } else {
+        let numOfGroceries = groceries.length;
+        return numOfGroceries + ' items'
+    }
+    
+}
 
 //////////////////Check out your code!////////////////////
 /*
