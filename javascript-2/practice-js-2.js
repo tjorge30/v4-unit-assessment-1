cb68d8104cd02e6dd538be3f9877f22e141ff9be
tjorge30 <mrs.javalera@gmail.com>
2021-02-19 16:37:29 -0800

    completed javascript-2

--- javascript-2/practice-js-2.js
+++ javascript-2/practice-js-2.js
@@ -10,7 +10,7 @@
 */
 
 //CODE HERE
-var gitDefinition = 'Git is version control system/ software.'
+var gitDefinition = 'Git is version control system/ software.';
 //////////////////PROBLEM 2////////////////////
 /*
     Create a variable called 'gitHubDefinition'.  
@@ -18,7 +18,7 @@ var gitDefinition = 'Git is version control system/ software.'
 */
 
 //CODE HERE
-var gitHubDefinition = 'GitHub is a cloudbased version control system.'
+let gitHubDefinition = 'GitHub is a cloudbased version control system.';
 //////////////////PROBLEMS 3 - 9////////////////////
 /*
     For the next several problems you will be creating objects containing information about different git 
@@ -34,9 +34,9 @@ var gitHubDefinition = 'GitHub is a cloudbased version control system.'
 */
 
 //CODE HERE
-var init = {
-    description: 'git init command creates a new Git repository. It can be used to convert an existing, unversioned project to a Git repository or initialize a new, empty repository.';
-    code: 'git init';
+let init = {
+    description: 'git init command creates a new Git repository. It can be used to convert an existing, unversioned project to a Git repository or initialize a new, empty repository.',
+    code: 'git init'
 }
 //////////////////PROBLEM 4////////////////////
 /*
@@ -45,7 +45,10 @@ var init = {
 */
 
 //CODE HERE
-
+let clone = {
+    description: 'Clones a repository into a newly created directory, creates remote-tracking branches for each branch in the cloned repository (visible using git branch --remotes), and creates and checks out an initial branch that is forked from the cloned repository’s currently active branch.',
+    code: 'git clone',
+}
 //////////////////PROBLEM 5////////////////////
 /*
     Create an object called 'status' with 'description' and 'code' properties 
@@ -53,7 +56,10 @@ var init = {
 */
 
 //CODE HERE
-
+let status = {
+    description: 'Displays paths that have differences between the index file and the current HEAD commit, paths that have differences between the working tree and the index file, and paths in the working tree that are not tracked by Git ',
+    code: 'git status'
+}
 //////////////////PROBLEM 6////////////////////
 /*
     Create an object called 'add' with 'description' and 'code' properties 
@@ -61,7 +67,10 @@ var init = {
 */
 
 //CODE HERE
-
+let add = {
+    description: 'This command updates the index using the current content found in the working tree, to prepare the content staged for the next commit. ',
+    code: 'git add'
+}
 //////////////////PROBLEM 7////////////////////
 /*
     Create an object called 'commit' with 'description' and 'code' properties
@@ -69,7 +78,10 @@ var init = {
 */
 
 //CODE HERE
-
+let commit = {
+    description: 'Create a new commit containing the current contents of the index and the given log message describing the changes. ',
+    code: 'git commit -m""'
+}
 //////////////////PROBLEM 8////////////////////
 /*
     Create an object called 'addRemote' with 'description' and 'code' properties
@@ -77,7 +89,10 @@ var init = {
 */
 
 //CODE HERE
-
+let addRemote = {
+    description: 'To add a new remote, use the git remote add command on the terminal, in the directory your repository is stored at.',
+    code: 'git remote add origin + url'
+}
 //////////////////PROBLEM 9////////////////////
 /*
     Create an object called 'push' with 'description' and 'code' properties 
@@ -85,3 +100,7 @@ var init = {
 */
 
 //CODE HERE
+let push = {
+    description: 'The git push command is used to upload local repository content to a remote repository.',
+    code: 'git push'
+}
